7752d813f7b2c700a6995faab048ebf53e6833a9
LFCKJ <k71689@naver.com>
2025-05-07 16:08:22 +0900

    오타 수정

--- src/component/Spinner/index.js
+++ src/component/Spinner/index.js
@@ -12,15 +12,15 @@ const Spinner = ({size = 24, color='#919EAB', loading= true,...props}) =>{
         height:size,
     };
     return loading ? <Icon>
-        <svg viewBox="0 0 38 38" xmlns="https://www.w3.org/2000/svg" style={sizeStyle}>
-            <g fill="none" fillRule="eenodd">
-                <g tranform="translate(1 1)">
-                    <path d="M36 18c0-9.94-8.06-18-18-18" stroke={color} strokWidth="2">
+        <svg viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" style={sizeStyle}>
+            <g fill="none" fillRule="evenodd">
+                <g transform="translate(1 1)">
+                    <path d="M36 18c0-9.94-8.06-18-18-18" stroke={color} strokeWidth="2">
                         <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite">
                         </animateTransform>                 
                     </path>
                         <circle fill={color} cx="36" cy="18" r="1">
-                            <animateTransform attributeName="transform" type="scale" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite">
+                            <animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite">
                             </animateTransform>
                         </circle>
                         
